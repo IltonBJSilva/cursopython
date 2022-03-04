2a25e83af022a1af396543485b01d23b3718dd94
ilton <xlsonplay@gmail.com>
2022-03-04 07:43:22 -0300

    Explicação de lista dentro da lista

--- Listas e enumerate/lista02.py
+++ Listas e enumerate/lista02.py
@@ -4,7 +4,14 @@ print(lista)
 #Resultado: [10.2, 13.75, 17.58, ['Maçã', 'Banana', 'Abacaxi’]]
 frutas = lista[3]
 
+
 print(frutas)
+'''
+Assim print(frutas) ou print(frutas[0]) na segunda opção
+se referencia por indice, ou seja vai aparecer apenas um dos resultados a cima da lista dentro
+da outra lista
+
+'''
 #Resultado: ['Maçã', 'Banana', 'Abacaxi’]
 soma = lista[0] + lista[1] + lista[2]
 
